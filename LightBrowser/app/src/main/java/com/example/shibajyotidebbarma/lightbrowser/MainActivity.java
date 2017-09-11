6a03c1c4d855bf6ad5dcca2fe5baff7e16fb1443
Shibajyoti Debbarma <debbarmaonit@gmail.com>
2017-09-11 20:50:08 +0530

    Basic UI implementations added

--- LightBrowser/app/src/main/java/com/example/shibajyotidebbarma/lightbrowser/MainActivity.java
+++ LightBrowser/app/src/main/java/com/example/shibajyotidebbarma/lightbrowser/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.shibajyotidebbarma.lightbrowser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView brow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        brow = (WebView) findViewById(R.id.webView_brow);
+        String url ="http://hackstories.com";
+
+        brow.loadUrl(url);
+
+
+
     }
 }
